f94217e6cedbbe0bdc9428e421dcf025e54ae0f5
Saiprasad Balasubramanian <sai.cosmo@gmail.com>
2020-08-08 17:17:34 +0530

    Added PATH to the start script

--- start-dash.sh
+++ start-dash.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PATH="$PATH:/home/pi/.rbenv/shims:/home/pi/.rbenv/bin"
+
 cd $DIR
-smashing start
+exec smashing start
